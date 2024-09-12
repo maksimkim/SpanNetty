bfbe8534218dbfd0a605a159913d627d79093a84
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-09-12 16:47:06 +0200

    try with older CI

--- test/DotNetty.Tests.Common/TestBase.cs
+++ test/DotNetty.Tests.Common/TestBase.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
+
 namespace DotNetty.Tests.Common
 {
   using DotNetty.Common.Internal.Logging;
@@ -14,6 +16,27 @@ namespace DotNetty.Tests.Common
     {
       this.Output = output;
       InternalLoggerFactory.DefaultFactory.AddProvider(new XUnitOutputLoggerProvider(output));
+      System.Diagnostics.Trace.Listeners.Add(new XUnitTraceListener(output));
+    }
+  }
+  
+  class XUnitTraceListener : TraceListener
+  {
+    readonly ITestOutputHelper _output;
+
+    public XUnitTraceListener(ITestOutputHelper outputHelper)
+    {
+      _output = outputHelper;
+    }
+
+    public override void Write(string message)
+    {
+      _output.WriteLine(message);
+    }
+
+    public override void WriteLine(string message)
+    {
+      _output.WriteLine(message);
     }
   }
 }
