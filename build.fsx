4b8078707b438d133fc39aa2820973049278f512
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-09-13 13:33:09 +0200

    sprintf

--- build.fsx
+++ build.fsx
@@ -242,7 +242,7 @@ Target "RunTests" (fun _ ->
                 info.WorkingDirectory <- (Directory.GetParent project).FullName
                 info.Arguments <- arguments) (TimeSpan.FromMinutes 30.0) 
         
-            log "Test Results: " + result
+            sprintf "Test Results: %s" result
             ResultHandling.failBuildIfXUnitReportedError TestRunnerErrorLevel.Error result
 
         CreateDir outputTests
