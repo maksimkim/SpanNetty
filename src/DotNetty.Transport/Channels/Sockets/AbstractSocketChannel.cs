74023776b131c5a0e2ab4a58e72edbf70a04c711
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-09-16 21:14:49 +0200

    just log it

--- src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
+++ src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
@@ -237,7 +237,7 @@ namespace DotNetty.Transport.Channels.Sockets
             IEventLoop eventLoop = channel.EventLoop;
             
 #if DEBUG
-            if (Logger.DebugEnabled) Logger.AbstractSocketIoCompleted(operation, channel, eventLoop);
+            Logger.AbstractSocketIoCompleted(operation, channel, eventLoop);
 #endif
             switch (args.LastOperation)
             {
