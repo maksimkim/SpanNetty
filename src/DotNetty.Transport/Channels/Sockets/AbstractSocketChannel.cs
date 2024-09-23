7b8ddb1b54985d76bc648d667d5535b2feb110dd
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-09-23 14:02:15 +0200

    in reverse

--- src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
+++ src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
@@ -237,7 +237,7 @@ namespace DotNetty.Transport.Channels.Sockets
             
             if (operation.SocketError == SocketError.OperationAborted // means System.Net.Sockets.Socket was closed. Most probably we received a callback for closure, not for real IO happened
                 && !channel.IsOpen // channel is already closed, meaning this is an expected closure
-                && !eventLoop.IsShuttingDown // if eventLoop itself is shutting down - we will never be able to schedule anything, and just throw exception 
+                && eventLoop.IsShuttingDown // if eventLoop itself is shutting down - we will never be able to schedule anything, and just throw exception 
             )
             {
                 if (Logger.InfoEnabled) Logger.AbstractSocketIoCallbackSkipped(operation, channel, eventLoop);
