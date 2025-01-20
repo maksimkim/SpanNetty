a3d68f86b7b45adfbfc71ed6ad2b3d87101ffbba
Maxim Kim <mkim@microsoft.com>
2025-01-19 21:57:13 -0800

    ignore socket io completion dispatch only for receive operation in case of both operationaborted and success

--- src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
+++ src/DotNetty.Transport/Channels/Sockets/AbstractSocketChannel.cs
@@ -235,9 +235,9 @@ namespace DotNetty.Transport.Channels.Sockets
             var @unsafe = channel.Unsafe;
             IEventLoop eventLoop = channel.EventLoop;
             
-            if (operation.SocketError == SocketError.OperationAborted // means System.Net.Sockets.Socket was closed. Most probably the callback was invoked because socket was closed, not because IO operation completed.
-                && !channel.IsOpen // channel is already closed, meaning this is an expected closure
-            )
+            if ((operation.LastOperation == SocketAsyncOperation.Receive || operation.LastOperation == SocketAsyncOperation.ReceiveFrom)
+                && (operation.SocketError == SocketError.OperationAborted || operation.SocketError == SocketError.Success) // means System.Net.Sockets.Socket was closed. Most probably the callback was invoked because socket was closed, not because IO operation completed.
+                && !channel.IsOpen) // channel is already closed, meaning this is an expected closure
             {
                 if (Logger.DebugEnabled) Logger.AbstractSocketIoCallbackSkipped(operation, channel);
                 return;
