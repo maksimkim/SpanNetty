a1a6987e35824a967bb9eea36eea6410b156f48e
Maxim Kim <mkim@microsoft.com>
2025-01-19 19:58:56 -0800

    log rejected operation in more socket tests

---
+++ test/DotNetty.Codecs.Http2.Tests/LoggingRejectionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using DotNetty.Common.Concurrency;
+using Xunit.Abstractions;
+
+namespace DotNetty.Codecs.Http2.Tests;
+
+public class LoggingRejectionHandler : IRejectedExecutionHandler
+{
+    private readonly ITestOutputHelper _output;
+
+    public LoggingRejectionHandler(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    public void Rejected(IRunnable task, SingleThreadEventExecutor executor)
+    {
+        if (task is AbstractExecutorService.StateActionWithContextTaskQueueNode node)
+        {
+            var socketEventArgs = node.State as SocketAsyncEventArgs;
+            _output.WriteLine($"Callback action scheduling rejected. Task type: {task.GetType()}, State type: {node.State?.GetType()}, Socket operation: {socketEventArgs?.LastOperation}, Socket error: {socketEventArgs?.SocketError}");
+        }
+                
+        throw new RejectedExecutionException();
+    }
+}
